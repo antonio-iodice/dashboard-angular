2c267c8626f5e1c467ea03a118b2c2d47ff56a04
Antonio Iodice <antonioiodice92@gmail.com>
2020-02-09 12:03:36 +0100

    Added backend

---
+++ api-backend/src/api/user.js
@@ -0,0 +1,34 @@
+const {Router} = require('express');
+const User = require('../persistence/users');
+
+const router = new Router();
+
+router.post('/', async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({message: 'email and password must be provided'});
+    }
+
+    const user = await User.create(email, password);
+    if (!user) {
+      return res.status(400).json({message: 'User already exists'});
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(
+      `createUser({ email: ${req.body.email} }) >> Error: ${error.stack}`
+    );
+    res.status(500).json();
+  }
+});
+
+router.get('/', (req, res) => {
+  console.log(req);
+  res.json({'ciao': req.query.user});
+});
+
+module.exports = router;
